7a05ef4db196e02cedb1778b37e2ace3beff522f
PaulZtx <36164890+PaulZtx@users.noreply.github.com>
2021-09-05 03:13:49 +0300

    Add files via upload
    if all the brackets are placed correctly, the script will output SUCCESS.If the brackets are placed incorrectly, the editor should also inform the user of the first place where the error was detected. First of all, you need to find a closing bracket for which either there is no corresponding opening one (for example, the bracket ] in the string"] ()” ), or it closes an opening bracket that does not correspond to it (example: “()[}”). If there are no such errors, it is necessary to find the first opening bracket for which there is no corresponding closing one (example: parenthesis ( in the string" {} ([]”).
    In addition to parentheses, the source code can contain Latin alphabet characters, numbers and punctuation marks.

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            CheckBrackets(str);
+        }
+
+        static void CheckBrackets(string str)
+        {
+            Stack<char> stack = new Stack<char>();
+            Stack<int> stack2 = new Stack<int>();
+            int count = 0;
+            
+            foreach (var st in str)
+            {
+                if (st != '(' && st != '[' && st != '{' &&
+                    st != ')' && st != ']' && st != '}')
+                {
+                    ++count;
+                    continue;
+                }
+
+                if (st == '(' || st == '[' || st == '{')
+                {
+                    stack.Push(st);
+                    ++count;
+                    stack2.Push(count);
+                    continue;
+                }
+
+                if (stack.Count == 0)
+                {
+                    Console.WriteLine(++count);
+                    return;
+                }
+                var peek = stack.Peek();
+
+                if (peek == '(' && st == ')'
+                    || peek == '{' && st == '}'
+                    || peek == '[' && st == ']')
+                {
+                    stack.Pop();
+                    stack2.Pop();
+                    ++count;
+                    continue;
+                }
+
+                if (peek == '(' && st != ')'
+                    || peek == '{' && st != '}'
+                    || peek == '[' && st != ']')
+                {
+                    Console.WriteLine(++count);
+                    return;
+                }
+            }
+
+            if (stack.Count != 0)
+                Console.WriteLine(stack2.Pop());
+            else Console.WriteLine("Success");
+        }
+    }
+}
